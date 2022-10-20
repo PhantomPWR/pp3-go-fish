8e82f422a48228c7d459eb290f24a17b75a85289
Jean de Villiers <jeandevilliers@me.com>
2022-10-20 18:26:42 +0000

    Map out functions

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
 
 
 def new_game():
+    """
+    Display game rules & instructions
+    Reset # books
+    Deal hands
+    Set stockpile
+    """
+
     # Clear screen
     print(chr(27) + "[2J")
     print("Go Fish!")
 
-    print("Function: new_game()")
+    print("Function running: new_game()")
 
 
 def game_rules():
@@ -14,7 +21,7 @@ def game_rules():
     - Instructions on how to play
     """
 
-    print("Function: game_rules()")
+    print("Function running: game_rules()")
 
 
 def play_game():
@@ -28,19 +35,88 @@ def play_game():
      - Card requests
      """
 
-     print("Function: play_game()")
+    print("Function running: play_game()")
+
+
+def player_hand():
+    """
+    - # Cards in hand
+    - Ranks & suits
+    - Contains books (4-of-a-kind)?
+    """
+
+    print("Function running: player_hand()")
 
 
 
-def  play_again():
+def player_books():
+    """
+    - # Books
+    - Which player has how many books
+    """
+
+    print("Function running: player_books()")
+
+
+def stock_pile():
+    """
+    - # Cards left
+    - Rank and suit of cards left
+    """
+
+    print("Function running: stock_pile()")
+
+
+def input_validation():
+    """
+    Check for:
+     - Format
+     - Case
+     - Range
+     """
+
+    print("Function running: input_validation()")
+
+
+def randomiser():
+    """
+    - Pick random card
+    - Check if card is available
+    """
+
+    print("Function running: randomiser()")
+
+
+def switch_player():
+    """
+    Switch active player after turn has finished
+    """
+
+    print("Function running: switch_player()")
+
+
+def play_again():
     """
     - Reset all
     - Exclude rules & instructions
     """
-    print("Function: play_again()")
+    print("Function running: play_again()")
 
 
 def main():
     """
     Runs all program functions
     """
+
+    new_game()
+    game_rules()
+    play_game()
+    player_hand()
+    player_books()
+    stock_pile()
+    input_validation()
+    randomiser()
+    switch_player()
+    play_again()
+
+main()
